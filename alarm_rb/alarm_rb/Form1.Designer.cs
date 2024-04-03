4efe10eb4de8ada493ab0b3edc8db183b1604907
Yui <112841826+YuitoFukunaga@users.noreply.github.com>
2024-04-03 17:05:28 +0900

    Add files via upload

---
+++ alarm_rb/alarm_rb/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace alarm_rb {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing&&(components!=null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.time.Location = new System.Drawing.Point(7, 30);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(105, 40);
+            this.time.TabIndex = 0;
+            this.time.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(118, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 103);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.time);
+            this.Name = "Form1";
+            this.Text = "アラームアプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ alarm_rb/alarm_rb/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alarm_rb {
+    public partial class Form1:Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        DateTime now = DateTime.Now;//現在時刻を秒間隔で格納する変数
+        SoundPlayer soundPlayer = new SoundPlayer("kaisi.wav");//時間になったら鳴らす音
+
+        /*
+         * 
+         * 起動時設定
+         *
+         */
+
+        /*
+         * 0 Sun 
+         * 1 Mon 
+         * 2 Tue 
+         * 3 Wed
+         * 4 Thu
+         * 5 Fri
+         * 6 Sat
+         */
+
+        int day_of_week;
+
+        /*set_alarm ={
+        *   {時間,コースの種類}
+        *}
+        *
+        *コースの種類
+        *0 : テラス,スプリンギン,プログラミング開始or終了
+        *1 : テラス,スプリンギン開始or終了
+        *2 : プログラミング開始or終了
+        *-1 : なし
+        */
+
+        int[,] set_startalarm = {
+            { 1600,0},{ 1710,1},{ 1740,2},{ 1820,1}
+        };
+
+        int[,] set_endalarm = {
+                {1700,1},{1730,2 },{1810,1 },{1910,2},{1920,1}
+        };
+        //1７３０
+        String[] alarm_message = { "テラス,スプリンギン,プログラミング","テラス,スプリンギン","プログラミング"};
+
+
+        /*
+         * 
+         * 起動時設定ここまで
+         *
+         */
+        
+        //秒を合わせるためのflag
+        Boolean second_flag = false;
+        //アラームを実行する曜日か判断するためのflag
+        Boolean day_of_week_alarm_flag = false;
+        //アラーム時間になっているかのflag
+        Boolean alarm_flag = false;
+
+        //起動時実行される関数
+        private void Form1_Load(object sender,EventArgs e) {
+            time.Text=now.ToString("HH:mm");//時刻を表示
+            day_of_week = ((int)(now.DayOfWeek));//曜日を設定
+            if(day_of_week == 1 || day_of_week==2 || day_of_week==3 || day_of_week==5){//アラームが必要な曜日か
+                day_of_week_alarm_flag = true;
+            }
+            timer1.Enabled=true;//タイマー起動
+            timer1.Interval=1000;//タイマー間隔を1秒に設定
+        }
+
+        //設定されている間隔で実行
+        private void timer1_Tick(object sender,EventArgs e) {
+            now=DateTime.Now;
+            //秒→分間隔にするための処理
+            if(!second_flag) {
+                if(int.Parse(now.ToString("ss")) == 0 ){
+                    timer1.Interval = 60000;//1分間隔に設定
+                    second_flag = true;
+                }
+            }
+            if(day_of_week_alarm_flag) {
+                for(int i = 0;i<set_startalarm.GetLength(1);i++) {
+                    if(int.Parse(now.ToString("HHmm"))==set_startalarm[i,0]) {
+                        label1.Text=alarm_message[set_startalarm[i,1]]+"開始";
+                        alarm_flag=true;
+                        soundPlayer.SoundLocation="kaisi.wav";
+                    }
+                }
+                for(int i = 0;i<set_endalarm.GetLength(1);i++) {
+                    if(int.Parse(now.ToString("HHmm"))==set_endalarm[i,0]) {
+                        label1.Text=alarm_message[set_endalarm[i,1]]+"終了";
+                        alarm_flag=true;
+                        soundPlayer.SoundLocation="syuuryou.wav";
+                    }
+                }
+
+                if(alarm_flag) {
+                    soundPlayer.Play();
+                    alarm_flag=false;
+                }
+            }
+            time.Text=now.ToString("HH:mm");
+        }
+
+        private void button1_Click(object sender,EventArgs e) {
+            soundPlayer.SoundLocation="kaisi.wav";
+            soundPlayer.Play();
+        }
+    }
+}
